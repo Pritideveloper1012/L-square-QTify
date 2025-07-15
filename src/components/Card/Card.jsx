5b839fb921a9959db55da58b3c406a2dd52cdc94
Priti Gupta <priticse2633@gmail.com>
2025-07-15 23:35:26 +0530

    Milestone 2 - Card and Section implemented

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,6 +3,7 @@ import styles from "./Card.module.css";
 import Chip from "@mui/material/Chip";
 
 function Card({ image, title, follows }) {
+  // console.log("Card props album:", album);
   return (
     <div className={styles.card}>
       <img src={image} alt={title} className={styles.cardImage} />
